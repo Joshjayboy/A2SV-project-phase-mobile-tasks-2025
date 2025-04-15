853b86efdda66cc1338877cac1e653d5eacf0129
joshjayboy <jakintemi@gmail.com>
2025-04-15 18:46:15 +0100

    complete mobile application

--- youtube/flutter_application_1/lib/learn_flutter_page.dart
+++ youtube/flutter_application_1/lib/learn_flutter_page.dart
@@ -8,6 +8,8 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool? isCheckbox = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,51 +22,89 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
           icon: const Icon(Icons.arrow_back_ios),
         ),
-      ),
-      body: Column(
-        children: [
-          Image.asset("images/unilag.jpg"),
-          const SizedBox(height: 10),
-          const Divider(color: Colors.black),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(10.0),
-            color: Colors.blueGrey,
-            width: double.infinity,
-            child: const Center(
-              child: Text(
-                "This is a text widget",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ),
-          ElevatedButton(
+        actions: [
+          IconButton(
             onPressed: () {
-              debugPrint("Elevated Button");
+              debugPrint("Actions");
             },
-            child: const Text("Elevated Button"),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint("Outlined Button");
-            },
-            child: const Text("Outlined Button"),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint("Text Button");
-            },
-            child: const Text("Text Button"),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Icon(Icons.local_fire_department),
-              Text("Row Widget"),
-              Icon(Icons.local_fire_department),
-            ],
+            icon: const Icon(Icons.info_outline),
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("images/unilag.jpg"),
+            const SizedBox(height: 10),
+            const Divider(color: Colors.black),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  "This is a text widget",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwitch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () {
+                debugPrint("Elevated Button");
+              },
+              child: const Text("Elevated Button"),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint("Outlined Button");
+              },
+              child: const Text("Outlined Button"),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint("Text Button");
+              },
+              child: const Text("Text Button"),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint("This is the row");
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(Icons.local_fire_department, color: Colors.blue),
+                  Text("Row Widget"),
+                  Icon(Icons.local_fire_department, color: Colors.blue),
+                ],
+              ),
+            ),
+            Switch(
+              value: isSwitch,
+              onChanged: (bool newBool) {
+                setState(() {
+                  isSwitch = newBool;
+                });
+              },
+            ),
+            Checkbox(
+              value: isCheckbox,
+              onChanged: (bool? newBool) {
+                setState(() {
+                  isCheckbox = newBool;
+                });
+              },
+            ),
+            Image.network(
+              "https://res.cloudinary.com/pro-solve/image/upload/v1744567843/FarmDev/rbs6cohj2xvvcwxtvmfr.jpg",
+            ),
+          ],
+        ),
       ),
     );
   }
